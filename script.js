a40b31051428859dd411794dd5fb40dfc67abf34
Shahnuma-Parvin <parvinshahnuma36@gmail.com>
2026-03-31 22:52:57 +0530

    Updated unlimited color changer project

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const start = document.getElementById('start');
+const stop = document.getElementById('stop');
 const h1 = document.querySelector('h1');
 
+let intervalId;
 start.addEventListener("click", () => {
-    setInterval(() =>{
+    console.log("Start button clicked");
+       
+       intervalId = setInterval(() =>{
        const r = Math.floor(Math.random() * 256);
        const g = Math.floor(Math.random() * 256);
        const b = Math.floor(Math.random() * 256);
@@ -10,4 +14,9 @@ start.addEventListener("click", () => {
        h1.innerHTML = `rgb(${r}, ${g}, ${b})`;
 
     },1000); 
+});
+
+stop.addEventListener("click", function(event){
+    console.log("Stop button clicked");
+    clearInterval(intervalId);
 });
